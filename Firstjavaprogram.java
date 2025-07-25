a5e26bfe49c4c498c336a36b506d95d6b56da483
sangrampatil9446-tech <sangrampatil9446@gmail.com>
2025-07-25 22:31:31 +0530

    Add files via upload

---
+++ Firstjavaprogram.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class Firstjavaprogram
+{ 
+	public static void main(String args[])
+	{
+		//System.out.println("Welcome to java");
+		//System.out.println(10+20);
+		
+		//practice---
+		System.out.println("welcome to java");
+		System.out.println(10+20);
+		
+		
+		
+	}
+
+	
+
+}
---
+++ Secondjavaprogram.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class Secondjavaprogram 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to java");
+
+       /* System.out.println("hello...");
+         System.out.println("hello...");
+         System.out.println("hello...");
+         System.out.println("hello...");
+         System.out.println("hello..."); 
+         */
+         
+	}
+
+}
